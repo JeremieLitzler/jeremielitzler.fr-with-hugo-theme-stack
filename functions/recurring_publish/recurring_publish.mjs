eacc82ca474688df721eb229637407d5482cda21
Jeremie Litzler <jeremie.litzler@gmail.com>
2024-03-25 07:55:51 +0100

    doc recurring publish fn

--- functions/recurring_publish/recurring_publish.mjs
+++ functions/recurring_publish/recurring_publish.mjs
@@ -1,45 +1,69 @@
+/**
+ * Import the schedule function to use cron expression
+ * for recurring functions.
+ */
 import { schedule } from "@netlify/functions";
 import { log } from "console";
-log("Starting recurring-publish function...");
-
+log("RECURRING_PUBLISH>Starting registering recurring-publish function...");
+/**
+ *
+ * @param {Object} event The event sent by Netlify CD
+ * @returns
+ */
 const handler = async (event) => {
+  log("RECURRING_PUBLISH>Starting function");
+  log("RECURRING_PUBLISH>event", event);
   /**
-   * Environment variables aren't retrieve via process.env
-   * but the global object `Netlify.env`.
+   * Environment variables are retrieved via process.env
+   * but not the global object `Netlify.env` as some docs
+   * suggests. Or it requires a certain package. The said-
+   * docs don't mention it.
    *
    * @see https://docs.netlify.com/functions/get-started/?fn-language=ts#environment-variables
+   *
+   * For setting Environment Variable, your app deploy settings and the
+   * "Environment Variables" blade.
+   *
+   * @see https://docs.netlify.com/configure-builds/environment-variables/
    */
-  log('process', process)
-  //log('Netlify', Netlify)
-  let RECURRING_BUILD_HOOK = null;
-  //if (Netlify === undefined) {
-   RECURRING_BUILD_HOOK = process.env.RECURRING_BUILD_HOOK; 
-  //} else {
-  // RECURRING_BUILD_HOOK = Netlify.env.get("RECURRING_BUILD_HOOK");
-  //}
-  log("Got RECURRING_BUILD_HOOK variable =>", RECURRING_BUILD_HOOK);
-  log("Fetching as POST the RECURRING_BUILD_HOOK...");
+  let RECURRING_BUILD_HOOK = process.env.RECURRING_BUILD_HOOK;
+  log(
+    "RECURRING_PUBLISH>Got RECURRING_BUILD_HOOK variable =>",
+    RECURRING_BUILD_HOOK
+  );
+  log("RECURRING_PUBLISH>Fetching as POST the RECURRING_BUILD_HOOK...");
   /**
    * Note: because functions use the standard Fetch API,
    * which was only added natively to Node.js in version
    * 18.0.0, no need for other libraries...
    *
+   * As of March 25th 2024, it should be fine.
    * @see https://docs.netlify.com/functions/get-started/?fn-language=ts#runtime
    */
   try {
     await fetch(RECURRING_BUILD_HOOK, { method: "POST" });
-    console.log("Fetch successed!");
+    log("RECURRING_PUBLISH>Build hook fetch success!");
     return {
       statusCode: 200,
       body: JSON.stringify({ message: `Build ran successfully.` }),
     };
   } catch (error) {
+    log("RECURRING_PUBLISH>Build hook fetch error!");
     return { statusCode: 500, body: error.toString() };
   }
 };
 
 let RECURRING_PUBLISH_CRON = process.env.RECURRING_PUBLISH_CRON;
-log("Got RECURRING_PUBLISH_CRON variable =>", RECURRING_PUBLISH_CRON);
+/**
+ * This was my attempt to use an environment variable to configure the cron.
+ * But Netlify build agent tells me it is not possible...
+ * Though the logs show the variable is read.
+ */
+log(
+  "RECURRING_PUBLISH>Got RECURRING_PUBLISH_CRON variable =>",
+  RECURRING_PUBLISH_CRON
+);
 //module.exports.handler = schedule(RECURRING_PUBLISH_CRON, handler);
 //module.exports.handler = schedule("*/5 * * * *", handler);//every 5 min
-module.exports.handler = schedule("0 4 * * *", handler);//every day at 4am GMT
+module.exports.handler = schedule("0 4 * * *", handler); //every day at 4am GMT
+log("RECURRING_PUBLISH>Done registering");
